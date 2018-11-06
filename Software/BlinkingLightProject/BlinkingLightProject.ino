3189de9b8756c6d364eb6eb2fe387d9bc84574c3
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-05 21:32:20 -0500

    working code on online simulator

--- Software/BlinkingLightProject/BlinkingLightProject.ino
+++ Software/BlinkingLightProject/BlinkingLightProject.ino
@@ -1,7 +1,7 @@
 // use preprocessor directives to define constants in the code
 #define FLASH_RATE_HZ 2
-#define BUTTON_IN 8
-#define PWM_LED_OUT 9
+#define BUTTON_IN 2
+#define PWM_LED_OUT 11
 #define PWM_MAX 255
 
 // operating mode: 0 - off, 1 - bright, 2 - mid-bright, 3 - dim, 4 - flash
@@ -16,7 +16,7 @@ void setup() {
   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(BUTTON_IN), button_pushed, FALLING);
   pinMode(PWM_LED_OUT, OUTPUT);
-
+  
 }
 
 void loop() {
@@ -24,18 +24,12 @@ void loop() {
   check_for_button_press();
   set_pwn_based_on_operating_mode();
   shine_led(PWM_OUT);
-
+  
 }
 
 void button_pushed() {
-
-  // Static variable only created once, the first time the function is called
-  static unsigned long previous_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
-  if (interrupt_time - previous_interrupt_time > 500) {
+  
     button_push = true;
-  }
-  previous_interrupt_time = interrupt_time;
 
 }
 
@@ -44,38 +38,29 @@ void set_pwn_based_on_operating_mode() {
   switch (operating_mode) {
     case 0:
       PWM_OUT = 0;
+      break;
     case 1:
       PWM_OUT = PWM_MAX;
+      break;
     case 2:
       PWM_OUT = int(PWM_MAX / 2);
+      break;
     case 3:
       PWM_OUT = int(PWM_MAX / 4);
+      break;
     case 4:
       flash_the_light();
+      break;
   }
 
 }
 
 void flash_the_light() {
 
-  static unsigned long previousT = 0;
-  unsigned long currentT = millis();
-  if (currentT - previousT >= (1000/FLASH_RATE_HZ)/2) {
-    // save the last time you blinked the LED
-    previousT = currentT;
-
-    // if the LED is off turn it on and vice-versa:
-    if (PWM_OUT == 0) {
-      PWM_OUT = 255;
-    } 
-    else if (PWM_OUT == 255) {
-      PWM_OUT = 0;
-    }
-  }
-  //digitalWrite(PWM_LED_OUT, HIGH);
-  //delay((1000 / FLASH_RATE_HZ) / 2);
-  //digitalWrite(PWM_LED_OUT, LOW);
-  //delay((1000 / FLASH_RATE_HZ) / 2);
+digitalWrite(PWM_LED_OUT, HIGH);
+delay((1000 / FLASH_RATE_HZ) / 2);
+digitalWrite(PWM_LED_OUT, LOW);
+delay((1000 / FLASH_RATE_HZ) / 2);
 
 }
 
@@ -86,7 +71,7 @@ void shine_led(int PWM_OUT) {
 }
 
 void check_for_button_press() {
-
+  
   if ((button_push) && (previous_mode != 4)) {
     operating_mode = previous_mode + 1;
     button_push = false;
@@ -95,6 +80,7 @@ void check_for_button_press() {
     operating_mode = 0;
     button_push = false;
   }
-
+  previous_mode = operating_mode;
+  
 }
 
